2a8e8b95953c1a5e65f1c52349f7027614e0bd94
melvin debot <melvindebot@MacBook-de-melvin.local>
2023-02-22 12:42:50 +0100

    :white_check_mark: add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import About from "./views/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +23,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
